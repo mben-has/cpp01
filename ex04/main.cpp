65d2e99cbfef74ad34b0a9588cf5975e953f3459
mben-has <mben-has@student.42heilbronn.de>
2024-03-03 01:37:10 +0100

    ex03

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,29 +6,51 @@
 /*   By: mben-has <mben-has@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 05:29:22 by mben-has          #+#    #+#             */
-/*   Updated: 2024/03/02 23:02:44 by mben-has         ###   ########.fr       */
+/*   Updated: 2024/03/03 01:36:42 by mben-has         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "Weapon.hpp"
-# include "HumanA.hpp"
-# include "HumanB.hpp"
-int main()
+# include <iostream>
+# include <fstream>
+
+int main(int argc, char **argv)
 {
+    if (argc != 4 || !argv[2][0] || !argv[3][0])
+	{
+		std::cout << "Program arguments error" << std::endl;
+		return (-1);
+	}
+    std::string filename = argv[1];
+    std::string outputFilename = filename + ".replace";
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    std::ifstream input(filename);
+    if (!input) 
     {
-    Weapon club = Weapon("crude spiked club");
-    HumanA bob("Bob", club);
-    bob.attack();
-    club.setType("some other type of club");
-    bob.attack();
+        std::cerr << "Error: Unable to open input file " << filename << std::endl;
+        return;
     }
+
+    std::ofstream output(outputFilename);
+    if (!output) 
     {
-    Weapon club = Weapon("crude spiked club");
-    HumanB jim("Jim");
-    jim.setWeapon(club);
-    jim.attack();
-    club.setType("some other type of club");
-    jim.attack();
+    std::cerr << "Error: Unable to create output file " << outputFilename << std::endl;
+    return;
     }
+    std::string line;
+     while (true) 
+     {
+        std::getline(input, line);
+        size_t pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos) {
+            line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+            pos += s2.length(); // Move position past the replaced string
+        }
+        output << line << '\n';
+    }
+
+    
 return 0;
 }
+
